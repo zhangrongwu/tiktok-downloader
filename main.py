ee01bb9d42934947f25991474105b082accb1c0d
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2021-05-31 05:34:46 +0200

    main.py added.

---
+++ main.py
@@ -0,0 +1,10 @@
+from package import downloader
+
+def main(username: str, save_path: str):
+    if username.startswith("@"):
+        username = username[1:]
+
+    downloader.download_profile(username, save_path)
+
+if __name__ == "__main__":
+    main("https://www.tiktok.com/@tiktokfunnyhub", "download/")
